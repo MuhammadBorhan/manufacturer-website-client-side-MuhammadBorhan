22a050a43bb46f5a418b8314a48489c9b64a6ea8
Muhammad Borhan <0317371981@student.wub.edu.bd>
2022-05-22 12:06:55 +0600

    navbar menu done

--- src/App.js
+++ src/App.js
@@ -1,10 +1,20 @@
+import { Route, Routes } from 'react-router-dom';
 import './App.css';
+import Home from './pages/Home/Home';
+import Login from './pages/LoginPage/Login';
+import Register from './pages/LoginPage/Register';
+import Navbar from './pages/Navbar/Navbar';
 
 function App() {
   return (
-    <div className="App">
-      <h1 className='text-green-500'>borhan uddin</h1>
-      <button class="btn btn-secondary">Two</button>
+    <div>
+      <Navbar></Navbar>
+      <Routes>
+        <Route path='/' element={<Home></Home>}></Route>
+        <Route path='/home' element={<Home></Home>}></Route>
+        <Route path='/login' element={<Login></Login>}></Route>
+        <Route path='/register' element={<Register></Register>}></Route>
+      </Routes>
     </div>
   );
 }

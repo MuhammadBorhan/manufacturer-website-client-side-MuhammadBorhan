eb275bead7d44aa8dd7af826e228b0b879e541d5
Muhammad Borhan <0317371981@student.wub.edu.bd>
2022-05-25 02:26:07 +0600

    add product route completed

--- src/pages/Dashboard/AddProducts.js
+++ src/pages/Dashboard/AddProducts.js
@@ -1,9 +1,51 @@
 import React from 'react';
+import { useAuthState } from 'react-firebase-hooks/auth';
+import auth from '../../Firebase/firebase.init';
 
 const AddProducts = () => {
+    const [user] = useAuthState(auth);
+
+    const handleAdded = event => {
+        event.preventDefault();
+        const name = event.target.name.value;
+        const email = event.target.email.value;
+        const price = event.target.price.value;
+        const minQuantity = event.target.minQuantity.value;
+        const avlbQuantity = event.target.avlbQuantity.value;
+        const description = event.target.description.value;
+        const img = event.target.img.value;
+
+        const addItem = { name, email, price, minQuantity, avlbQuantity, description, img };
+        const url = `http://localhost:5000/product`;
+        fetch(url, {
+            method: "POST",
+            headers: {
+                "content-type": "application/json"
+            },
+            body: JSON.stringify(addItem)
+        })
+            .then(res => res.json())
+            .then(result => {
+                console.log(result);
+                event.target.reset()
+            })
+    }
+
     return (
         <div>
-            <h1>Add products</h1>
+            <h1 className='mt-5 mb-3 text-center text-green-600 text-3xl font-bold'>Add products</h1>
+            <div>
+                <form onSubmit={handleAdded} className='grid grid-cols-1 gap-4 justify-items-center'>
+                    <input type="text" name='name' placeholder="Product Name" class="input input-bordered w-full max-w-xs" />
+                    <input type="email" name='email' value={user.email} class="input input-bordered w-full max-w-xs" />
+                    <input type="text" name='price' placeholder="Price" class="input input-bordered w-full max-w-xs" />
+                    <input type="text" name='minQuantity' placeholder="MinQuantity" class="input input-bordered w-full max-w-xs" />
+                    <input type="text" name='avlbQuantity' placeholder="AvlbQuantity" class="input input-bordered w-full max-w-xs" />
+                    <textarea name='description' class="textarea textarea-bordered w-full max-w-xs" placeholder="Description"></textarea>
+                    <input type="text" name='img' placeholder="Photo URL" class="input input-bordered w-full max-w-xs" />
+                    <input type="submit" value='Add Item' class="btn input-bordered w-full max-w-xs" />
+                </form>
+            </div>
         </div>
     );
 };
--- src/pages/Dashboard/Dashboard.js
+++ src/pages/Dashboard/Dashboard.js
@@ -22,7 +22,7 @@ const Dashboard = () => {
                         <button className='btn btn-accent text-white font-bold'><li><Link to='/dashboard/myprofile'>My Profile</Link></li></button>
                         <button className='btn btn-success my-2 text-white font-bold'><li><Link to='/dashboard/allorder'>All OrderS</Link></li></button>
                         <button className='btn btn-secondary font-bold'><li><Link to='/dashboard/addproducts'>Add Products</Link></li></button>
-                        <button className='btn btn-accent mt-2 text-white font-bold'><li><Link to='/dashboard/allproducts'>Manage All Products</Link></li></button>
+                        <button className='btn mt-2 text-white font-bold'><li><Link to='/dashboard/allproducts'>Manage All Products</Link></li></button>
                     </ul>
 
                 </div>

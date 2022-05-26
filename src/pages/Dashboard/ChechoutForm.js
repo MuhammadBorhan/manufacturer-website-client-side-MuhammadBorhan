7da4395c43f21bfd8638a004b4e2e57170e18f38
Muhammad Borhan <0317371981@student.wub.edu.bd>
2022-05-26 20:08:06 +0600

    payment methods created

--- src/pages/Dashboard/ChechoutForm.js
+++ src/pages/Dashboard/ChechoutForm.js
@@ -1,9 +1,15 @@
 import { CardElement, useElements, useStripe } from '@stripe/react-stripe-js';
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 
 const ChechoutForm = () => {
     const stripe = useStripe()
     const element = useElements()
+    const [cardError, setCardError] = useState('');
+    const [clientSecret, setClientSecret] = useState('');
+    useEffect(() => {
+
+    }, []);
+
     const handleSubmit = async (event) => {
         event.preventDefault();
         if (!stripe || !element) {
@@ -14,29 +20,45 @@ const ChechoutForm = () => {
             return;
         }
 
+        const { error, paymentMethod } = await stripe.createPaymentMethod({
+            type: 'card',
+            card
+        });
+        if (error) {
+            setCardError(error.message)
+        }
+        else {
+            setCardError('');
+        }
+
     }
     return (
-        <form onSubmit={handleSubmit}>
-            <CardElement
-                options={{
-                    style: {
-                        base: {
-                            fontSize: '16px',
-                            color: '#424770',
-                            '::placeholder': {
-                                color: '#aab7c4',
+        <>
+            <form onSubmit={handleSubmit}>
+                <CardElement
+                    options={{
+                        style: {
+                            base: {
+                                fontSize: '16px',
+                                color: '#424770',
+                                '::placeholder': {
+                                    color: '#aab7c4',
+                                },
+                            },
+                            invalid: {
+                                color: '#9e2146',
                             },
                         },
-                        invalid: {
-                            color: '#9e2146',
-                        },
-                    },
-                }}
-            />
-            <button className='btn btn-success btn-sm mt-4' type="submit" disabled={!stripe}>
-                Pay
-            </button>
-        </form>
+                    }}
+                />
+                <button className='btn btn-success btn-sm mt-4' type="submit" disabled={!stripe}>
+                    Pay
+                </button>
+            </form>
+            {
+                cardError && <p className='text-red-500'>{cardError}</p>
+            }
+        </>
     );
 };
 

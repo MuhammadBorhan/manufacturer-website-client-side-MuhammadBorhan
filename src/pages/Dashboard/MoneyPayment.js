dd111fc5e0e1ff99c45f9e1423afac930dbca7ee
Muhammad Borhan <0317371981@student.wub.edu.bd>
2022-05-26 19:26:16 +0600

    pay system done

--- src/pages/Dashboard/MoneyPayment.js
+++ src/pages/Dashboard/MoneyPayment.js
@@ -1,7 +1,13 @@
+import { Elements } from '@stripe/react-stripe-js';
+import { loadStripe } from '@stripe/stripe-js';
 import React from 'react';
 import { useQuery } from 'react-query';
 import { useParams } from 'react-router-dom';
 import Loading from '../Loading/Loading';
+import ChechoutForm from './ChechoutForm';
+
+const stripePromise = loadStripe('pk_test_51L3gF3GgLrvBkIM2fM9eN5YIggt2tGBxWwoDz2GxFSJ8a0uru4CIJoIeHjVZoWAEf0T5c2EM4YS5GU9z3N1LpGMk00ztDodrvr');
+
 
 const MoneyPayment = () => {
     const { id } = useParams();
@@ -11,9 +17,24 @@ const MoneyPayment = () => {
         return <Loading></Loading>
     }
     return (
+
         <div>
-            <h1>Payment for: {order.name}</h1>
+            <div class="card w-50 max-w-md bg-base-100 shadow-xl my-12">
+                <div class="card-body">
+                    <h2 className='text-primary text-3xl font-bold'>Hello {order.name}</h2>
+                    <h2 class="card-title">Pay for {order.product}</h2>
+                    <p>Please Pay: $</p>
+                </div>
+            </div>
+            <div class="card flex-shrink-0 w-50 max-w-md shadow-2xl bg-base-100">
+                <div class="card-body">
+                    <Elements stripe={stripePromise}>
+                        <ChechoutForm></ChechoutForm>
+                    </Elements>
+                </div>
+            </div>
         </div>
+
     );
 };
 

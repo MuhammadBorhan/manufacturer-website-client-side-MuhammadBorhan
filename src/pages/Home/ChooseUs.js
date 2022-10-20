653a5191bad607b76bd9750f25d7dca349172ccc
MuhammadBorhan <0317371981@student.wub.edu.bd>
2022-10-20 13:00:17 +0600

    title updated

--- src/pages/Home/ChooseUs.js
+++ src/pages/Home/ChooseUs.js
@@ -1,8 +1,9 @@
-import React from 'react';
+import React, { useState } from 'react';
 import CountUp from 'react-countup';
 import ScrollTrigger from 'react-scroll-trigger';
 
 const ChooseUs = () => {
+    const [counterOn,setCounterOn]=useState(false);
     return (
         <div className='py-8' 
         data-aos="fade-down"
@@ -13,11 +14,13 @@ const ChooseUs = () => {
                 <h2 className='text-indigo-600 text-3xl font-bold mb-5'>Why Choose Us</h2>
                 <p className='text-xl'>Automotive parts manufacturing utilizes ongoing employee training, automation, continuous process improvement and innovation to maximize the quantity, reliability, and cost-effectiveness of your components. We are uniquely positioned to address your most challenging product part approval process and failure modes and effects component mandates.</p>
             </div>
+            <ScrollTrigger onEnter={()=>setCounterOn(true)} onExit={()=>setCounterOn(false)}>
             <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 text-center px-12 mt-5'>
-                <p className='shadow-lg py-3'><span className='text-green-700 text-4xl font-bold'>6</span> <br /> <span className='text-xl font-bold'>Years Business</span></p>
-                <p className='shadow-lg py-3 mx-0 lg:mx-3'><span className='text-green-700 text-4xl font-bold'>1,760</span> <br /> <span className='text-xl font-bold'>Square Foot Facility</span></p>
-                <p className='shadow-lg py-3'><span className='text-green-700 text-4xl font-bold'>44</span> <br /> <span className='text-xl font-bold'>Employees</span></p>
+                <p className='shadow-lg py-3'><span className='text-green-700 text-4xl font-bold'>{counterOn&&<CountUp start={0} end={6} duration={2} delay={0}/>}</span> <br /> <span className='text-xl font-bold'>Years Business</span></p>
+                <p className='shadow-lg py-3 mx-0 lg:mx-3'><span className='text-green-700 text-4xl font-bold'>{counterOn&&<CountUp start={0} end={1760} duration={3} delay={0}/>}</span> <br /> <span className='text-xl font-bold'>Square Foot Facility</span></p>
+                <p className='shadow-lg py-3'><span className='text-green-700 text-4xl font-bold'>{counterOn&&<CountUp start={0} end={44} duration={2} delay={0}/>}</span> <br /> <span className='text-xl font-bold'>Employees</span></p>
             </div>
+            </ScrollTrigger>
         </div>
     );
 };
--- src/pages/Home/Summary.js
+++ src/pages/Home/Summary.js
@@ -15,17 +15,17 @@ const Summary = () => {
           <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 text-center py-8'>
                 <div>
                     <p className='text-6xl text-blue-400 flex justify-center'><FaHandshake /></p>
-                    <p className='text-5xl font-bold my-4'> {counterOn&&<CountUp start={0} end={50} duration={2} delay={0}/>}+</p>
+                    <p className='text-5xl font-bold my-4'> {counterOn&&<CountUp start={0} end={50} duration={2.5} delay={0}/>}+</p>
                     <p className='text-xl text-blue-400 font-bold'>Served</p>
                 </div>
                 <div className='py-12 lg:py-0'>
                     <p className='text-6xl text-blue-400 flex justify-center'><FaMoneyBillAlt /></p>
-                    <p className='text-5xl font-bold my-4'>{counterOn&&<CountUp start={0} end={35} duration={0.8} delay={0}/>}M+</p>
+                    <p className='text-5xl font-bold my-4'>{counterOn&&<CountUp start={0} end={35} duration={2} delay={0}/>}M+</p>
                     <p className='text-xl text-blue-400 font-bold'>Annual Revenue</p>
                 </div>
                 <div className='pb-12 lg:pb-0'>
                     <p className='text-6xl text-blue-400 flex justify-center'><FaCarCrash /></p>
-                    <p className='text-5xl font-bold my-4'>{counterOn&&<CountUp start={0} end={15} duration={0.5} delay={0}/>}+</p>
+                    <p className='text-5xl font-bold my-4'>{counterOn&&<CountUp start={0} end={15} duration={2} delay={0}/>}+</p>
                     <p className='text-xl text-blue-400 font-bold'>Car Parts</p>
                 </div>
                 <div>
